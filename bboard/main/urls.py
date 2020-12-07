c527e69004b1d42a6fb8947ca97859ccdcee8857
wiky-avis <peta-holding@yandex.ru>
2020-12-07 22:00:57 +0300

    add profile user

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index')
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 def index(request):
@@ -19,3 +22,11 @@ def other_page(request, page):
 
 class BBLoginView(LoginView):
     template_name = 'main/login.html'
+
+
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
